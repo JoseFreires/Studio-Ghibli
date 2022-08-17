37cd59c7e9f9f7a01c376459dd4d313b5fdb0000
Jose_Freires <88195769+JoseFreires@users.noreply.github.com>
2022-08-16 21:08:58 -0300

    Add files via upload

---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './styleFooter.css'
+import logo_git from '../../assets/logo-github.png'
+import logo_linkedin from '../../assets/linkedin.png'
+
+export default function Footer(){
+    return(
+        <div className='containerFooter'>
+            <div>
+                <h1>Feito por José Freires</h1>
+            </div>
+            <div className='containerLogo'>
+                <a href='https://github.com/JoseFreires' target='blank'>
+                    <img id='logo-footer' src={logo_git} alt="Logo Github"/>
+                </a>
+                <a href='https://www.linkedin.com/in/josé-guilherme-silva-freires-27b778227?original_referer=' target='blank'>
+                    <img id='logo-footer' src={logo_linkedin} alt="Logo Linkedin"/>
+                </a>
+            </div>
+        </div>
+    )
+}
